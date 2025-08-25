894df90aa5f9c87c7b5a78bf881859638306487b
Leonardo Esperança <dev.esperanca.leonardo@gmail.com>
2025-08-24 21:09:33 -0300

    💄 Replace tokyonight theme with monokai-pro setup
    - Replace tokyonight theme with monokai-pro setup
    - Add indent-blankline.nvim with rainbow-colored indentation lines
    - Improve readability with visually distinct indent guides
    - Ensure highlights refresh automatically when theme changes

--- nvim/init.lua
+++ nvim/init.lua
@@ -34,18 +34,63 @@ vim.opt.rtp:prepend(lazypath)
 -- Plugins
 ------------------------------------------------------------
 require("lazy").setup({
+------------------------------------------------------------
+-- Ident Blankline
+------------------------------------------------------------
+  {
+  "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
+  config = function()
+    local highlight = {
+      "RainbowRed",
+      "RainbowYellow",
+      "RainbowBlue",
+      "RainbowOrange",
+      "RainbowGreen",
+      "RainbowViolet",
+      "RainbowCyan",
+    }
+
+    local hooks = require "ibl.hooks"
+    -- recria os highlights sempre que o tema mudar
+    hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+      vim.api.nvim_set_hl(0, "RainbowRed",    { fg = "#E06C75" })
+      vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+      vim.api.nvim_set_hl(0, "RainbowBlue",   { fg = "#61AFEF" })
+      vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+      vim.api.nvim_set_hl(0, "RainbowGreen",  { fg = "#98C379" })
+      vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+      vim.api.nvim_set_hl(0, "RainbowCyan",   { fg = "#56B6C2" })
+    end)
+
+    require("ibl").setup({
+      indent = { highlight = highlight },
+    })
+  end,
+  },
+
   ----------------------------------------------------------------
   -- Tema
   ----------------------------------------------------------------
+  --{
+    --"folke/tokyonight.nvim",
+    --lazy = false,
+    --priority = 1000,
+    --config = function()
+      --vim.cmd.colorscheme("tokyonight")
+    --end,
+  --},
   {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme("tokyonight")
-    end,
+  "loctvl842/monokai-pro.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require("monokai-pro").setup({
+      filter = "pro", -- opções: pro, classic, octagon, machine, ristretto, spectrum
+    })
+    vim.cmd.colorscheme("monokai-pro")
+  end,
   },
-
   ----------------------------------------------------------------
   -- Ícones (usado por vários plugins)
   ----------------------------------------------------------------
